8f9dd2a29e16bd155a206daf956b6e546e2d4766
elenabochkova <planeado@gmail.com>
2020-05-09 14:05:44 +0300

    Added intellectual fixture

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,10 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Elena",
                                lastname="Bochkova",
                                nickname="alef",
                                home_phone="7(900)9009090"))
-    app.session.logout()
 
 
 
