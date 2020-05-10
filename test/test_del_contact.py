a87b3c4e18fc38393a0977c6e4e2c218d38f03c1
elenabochkova <planeado@gmail.com>
2020-05-10 13:59:12 +0300

    Adds preconditions to delete and modify tests

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,3 +1,10 @@
+from model.contact import Contact
+
 
 def test_delete_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="New",
+                               lastname="Contact",
+                               nickname="test",
+                               home_phone="7(900)9009090"))
     app.contact.delete_first()
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,6 +2,11 @@ from model.contact import Contact
 
 
 def test_modify_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="New",
+                               lastname="Contact",
+                               nickname="test",
+                               home_phone="7(900)9009090"))
     app.contact.modify_first(Contact(firstname="Boris",
                                lastname="Grebenschikov",
                                nickname="modified",
