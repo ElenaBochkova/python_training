37997754ecc04681c6b87c7fc04899a51ac90ae3
elenabochkova <planeado@gmail.com>
2020-05-17 15:46:47 +0300

    Added cashes to group tests

--- fixture/group.py
+++ fixture/group.py
@@ -5,15 +5,18 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    group_cache = None
+
     def get_group_list(self):
-        wd = self.app.wd
-        self.open_groups_page()
-        groups = []
-        for element in wd.find_elements_by_css_selector("span.group"):
-            text = element.text
-            id = element.find_element_by_name("selected[]").get_attribute("value")
-            groups.append(Group(name = text, id = id))
-        return groups
+        if self.group_cache is None:
+            wd = self.app.wd
+            self.open_groups_page()
+            self.group_cache = []
+            for element in wd.find_elements_by_css_selector("span.group"):
+                text = element.text
+                id = element.find_element_by_name("selected[]").get_attribute("value")
+                self.group_cache.append(Group(name = text, id = id))
+        return list(self.group_cache)
 
     def count(self):
         wd = self.app.wd
@@ -28,6 +31,7 @@ class GroupHelper:
         self.fill_group(group)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def select_first_group(self):
         wd = self.app.wd
@@ -40,6 +44,7 @@ class GroupHelper:
         #submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def return_to_groups_page(self):
         wd = self.app.wd
@@ -55,6 +60,7 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+        self.group_cache = None
 
     def fill_group(self, group):
         self.change_field_value("group_name", group.name)
